f56526e6d64757461432ade5ad27e9d81b4b35ac
Alejandra Torres <72315459+mariameraz@users.noreply.github.com>
2021-09-28 13:08:34 -0500

    Update DEG.R

--- DEG.R
+++ DEG.R
@@ -1,13 +1,35 @@
 # Load libraries 
-library(dplyr)
-library(tidyverse)
-library(edgeR)
-library(lemon)
-library(ggvenn)
-library(ComplexHeatmap)
-library(viridis)
-library(ggpubr)
-library(gridExtra)
+#library(dplyr)
+#library(tidyverse)
+#library(edgeR)
+#library(lemon)
+#library(ggvenn)
+#library(ComplexHeatmap)
+#library(viridis)
+#library(ggpubr)
+#library(gridExtra)
+
+CheckPackages <-
+  function(x){
+          for (i in length(x)) {
+            if (x[i] %in% installed.packages()[,"Package"]) { 
+                   library(x[i], character.only = T) 
+              } else {
+                    BiocManager::install(x[i]) 
+                
+                if (x[i] %in% installed.packages()[,"Package"]) { 
+                  library(x[i], character.only = T) 
+                } else {
+                  install.package(x[i]) 
+                }
+              
+              }
+          }
+}
+
+pack.list <- c("dplyr","tidyverse", "edgeR","lemon","ggvenn","ComplexHeatmap","viridis", "ggpubr", "gridExtra")
+
+CheckPackages(x=pack.list)
 
 setwd("./Cranberry_transcriptomics/") #Set working directory
 
