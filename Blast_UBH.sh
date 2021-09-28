c8c297341b86eeb9f7e38ed4e5d8d2a4229e04f9
Alejandra Torres <72315459+mariameraz@users.noreply.github.com>
2021-09-28 10:39:25 -0500

    Create Blast_UBH.sh

---
+++ Blast_UBH.sh
@@ -0,0 +1,15 @@
+#PBS -N blast_ubh
+#PBS -q default
+#PBS -l nodes=1:ppn=1,walltime=99999:999:00,mem=30gb,vmem=30gb
+#PBS -V
+
+cd $PBS_O_WORKDIR
+
+module load ncbi-blast+/2.6.0
+
+blastp -query ../uniprot.fasta\
+ -db $PATH/macrocarpon.proteins.fasta -outfmt 7\
+ -max_target_seqs 10 -evalue 1e-5 -out uniprot_vs_macro_UBH.tab #UBH:"Undirectional best hits".
+ 
+#Filter best uniprot hits IDs
+grep 'sp|' macrocarpon_to_uniprot.tab | cut -f 2 > uniprot_vs_macro_UBH.txt
