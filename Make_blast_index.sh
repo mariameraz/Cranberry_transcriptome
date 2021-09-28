3dcf6fca2f97d187727b306d9c041b5c5b0e38cd
Alejandra Torres <72315459+mariameraz@users.noreply.github.com>
2021-09-28 10:26:39 -0500

    Create Make_blast_index.sh

---
+++ Make_blast_index.sh
@@ -0,0 +1,10 @@
+#PBS -N blast_idx
+#PBS -q default
+#PBS -l nodes=1:ppn=1,walltime=999:999:00,mem=30gb,vmem=30gb
+#PBS -V
+
+cd $PBS_O_WORKDIR
+
+module load ncbi-blast+/2.6.0
+
+makeblastdb -in macrocarpon.proteins.fasta -dbtype prot
